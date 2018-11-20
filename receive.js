5eebf46196f92429ab2859508690c2527e2a24f6
srinivasrk <srinivasrk92@gmail.com>
2018-11-20 11:05:21 -0500

    hello world example

---
+++ receive.js
@@ -0,0 +1,22 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect({
+  protocol: 'amqp',
+  hostname: `${process.env.RABBITMQ_SERVER}`,
+  port: 5672,
+  username: 'srini',
+  password: 'srini',
+  locale: 'en_US',
+  frameMax: 0,
+  heartbeat: 0
+}, function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {noAck: true});
+  });
+});
---
+++ send.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect({
+  protocol: 'amqp',
+  hostname: `${process.env.RABBITMQ_SERVER}`,
+  port: 5672,
+  username: 'srini',
+  password: 'srini',
+  locale: 'en_US',
+  frameMax: 0,
+  heartbeat: 0
+}, function(err, conn) {
+  if(err) {
+    console.log(`${err}`);
+    process.exit(0)
+  }
+  conn.createChannel(function(err, ch) {
+    var q = 'hello'
+
+    ch.assertQueue(q, {durable : false});
+    ch.sendToQueue(q, new Buffer('Hello World!'));
+    console.log(" [x] Sent 'Hello World!'");
+  })
+  // END AFTER 0.5 SECONDS
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+})
